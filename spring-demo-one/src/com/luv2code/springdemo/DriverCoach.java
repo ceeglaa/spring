f1f512c442a88ee95470959f7c92f5ddb43d7876
Adrian Cegłowski <s16028@pjwstk.edu.pl>
2019-09-03 22:37:17 +0200

    change kilometers for driver

--- spring-demo-one/src/com/luv2code/springdemo/DriverCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/DriverCoach.java
@@ -5,7 +5,7 @@ public class DriverCoach implements Coach{
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
-		return "Drive 400 km every day";
+		return "Drive 500 km every day";
 	}
 
 }
