ed88b90a11ebe71d32ee5eb6ec96ac702f1af048
ceeglaa <ceeglaa@gmail.com>
2019-09-04 20:46:00 +0200

    Revert "delete brick cooach"
    This reverts commit 5cad98e708ea1ca5719c974fc42e451ccf00d36d.

---
+++ spring-demo-one/src/com/luv2code/springdemo/BrickCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class BrickCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Write spring code 2 hours every day ;)";
+	}
+
+}
