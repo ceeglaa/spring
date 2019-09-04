f3b67ae981623c18b9697bb7d9cfc93a3e2788c6
ceeglaa <ceeglaa@gmail.com>
2019-09-04 21:44:06 +0200

    new coach created

---
+++ spring-demo-one/src/com/luv2code/springdemo/EsportCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class EsportCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Play Leagur of legends 15 hours every day";
+	}
+
+}
--- spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -15,11 +15,13 @@ public class HelloSpringApp {
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		Coach brickCoach = context.getBean("brickCoach", Coach.class);
 		Coach driverCoach = context.getBean("driverCoach", Coach.class);
+		Coach esportCoach = context.getBean("esportCoach", Coach.class);
 		
 		//call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(brickCoach.getDailyWorkout());
 		System.out.println(driverCoach.getDailyWorkout());
+		System.out.println(esportCoach.getDailyWorkout());
 		
 		//close the context
 		context.close();
