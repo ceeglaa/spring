b154bcc83f3ee83fa4f0cadf3839b3004e67855c
ceeglaa <ceeglaa@gmail.com>
2019-09-05 00:06:27 +0200

    learning dependency injection - constructor dependency

--- spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -12,17 +12,12 @@ public class HelloSpringApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//retreive bean from spring container
-		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach brickCoach = context.getBean("brickCoach", Coach.class);
-		Coach driverCoach = context.getBean("driverCoach", Coach.class);
-		Coach esportCoach = context.getBean("esportCoach", Coach.class);
+		Coach baseballCoach = context.getBean("baseballCoach", Coach.class);
+		
 		
 		//call methods on bean
-		System.out.println(theCoach.getDailyWorkout());
-		System.out.println(brickCoach.getDailyWorkout());
-		System.out.println(driverCoach.getDailyWorkout());
-		System.out.println(esportCoach.getDailyWorkout());
-		
+		System.out.println(baseballCoach.getDailyFortune());
+		System.out.println(baseballCoach.getDailyWorkout());
 		//close the context
 		context.close();
 
